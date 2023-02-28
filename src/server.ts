dc3f3d39b6caa30794db9f3d7367ae6260839631
Pedro Bovolin D' Avila <pedrodavila.bovolin@gmail.com>
2023-02-28 18:36:26 -0300

    Validate enviroment variables with zod

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from 'fastify'
 // import { randomUUID } from 'node:crypto'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
@@ -14,6 +15,6 @@ app.get('/hello', async () => {
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
-  .then(() => console.log('Server on.'))
+  .then(() => console.log(`Server on - Port: ${env.PORT}.`))
